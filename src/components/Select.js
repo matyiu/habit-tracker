89d32f791c0bdc81a0e056fe9bd4458194320a34
Jefferson Valle <valleespinozajefferson@gmail.com>
2019-10-01 17:32:52 +0200

    onChange Event Handler to Select component

--- src/components/Select.js
+++ src/components/Select.js
@@ -5,7 +5,8 @@ function Select(props) {
     return (
         <div className="select" style={{width: props.width}}>
             <select className={"select__input" 
-            + (props.className ? ' ' + props.className : '')}>
+            + (props.className ? ' ' + props.className : '')}
+            onChange={props.onChange}>
                 {props.children}
             </select>
             <i class="select__icon fas fa-angle-down"></i>
