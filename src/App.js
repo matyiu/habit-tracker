ca8c34e902fafb33c00b3bb912730b48fd2d569c
Jefferson Valle <valleespinozajefferson@gmail.com>
2019-10-17 20:48:03 +0200

    Moved habits from app to HabitList component

--- src/App.js
+++ src/App.js
@@ -1,85 +1,37 @@
 import React from 'react';
 import './App.scss';
 import HabitForm from './HabitForm';
-import Habit from './components/Habit';
-import HabitList from './storage/habit';
-import HabitOptions from './components/dropdown/HabitOptions';
+import HabitStorage from './storage/habit';
+import HabitList from "./components/habit/HabitList";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.habitList = new HabitList();
+    this.habitStorage = new HabitStorage();
     this.state = {
-      habits: this.habitList.get(),
-      dropdown: false,
-      activeCoords: {}
+      habits: this.habitStorage.get()
     }
 
     this.update = this.update.bind(this);
-    this.displayHabitOptions = this.displayHabitOptions.bind(this);
-    this.dropdownClickOutside = this.dropdownClickOutside.bind(this);
     this.setId = this.setId.bind(this);
   }
 
   update() {
     this.setState({
-      habits: this.habitList.get()
+      habits: this.habitStorage.get()
     });
-  }
-
-  displayHabitOptions(e) {
-    e.stopPropagation();
-    this.setState({ dropdown: true });
-    const icon = e.currentTarget.querySelector('i');
-    const x = icon.getBoundingClientRect().x;
-    const y = icon.getBoundingClientRect().y;
-    const top = y + icon.offsetHeight + 5;
-    const right = window.innerWidth - x - icon.offsetWidth;
-    if (top === this.state.activeCoords.top && this.state.dropdown) {
-      this.setState({ dropdown: false });
-      return;
-    }
-    this.setState({ activeCoords: { top, right } });
-  }
-
-  dropdownClickOutside() {
-    if (this.state.dropdown) {
-      this.setState({ dropdown: false });
-    }
   }
 
   setId(id) {
     this.setState({ id });
   }
 
-  componentDidMount() {
-    window.addEventListener('click', this.dropdownClickOutside);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('click', this.dropdownClickOutside)
-  }
-
   render() {
     return (
       <>
-        <HabitForm storage={this.habitList} update={this.update} />
-        <div className="habit-list">
-          <div className="container">
-            <div className="row row--column">
-              { this.state.dropdown &&
-              <HabitOptions 
-              storage={this.habitList}
-              update={this.update}
-              coords={this.state.activeCoords}
-              id={this.state.id} /> }
-              {this.state.habits.map(habit => 
-              <Habit key={ habit.id } habitOptions={habit}
-              displayHabitOptions={this.displayHabitOptions}
-              setId={this.setId} />)}
-            </div>
-          </div>
-        </div>
+        <HabitForm storage={this.habitStorage} update={this.update} />
+        <HabitList storage={this.habitStorage} update={this.update}
+        id={this.state.id} habits={this.state.habits} setId={this.setId} />
       </>
     );
   }
---
+++ src/components/habit/HabitList.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Habit from '../Habit';
+import HabitOptions from '../dropdown/HabitOptions';
+
+class HabitList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dropdown: false,
+            activeCoords: {}
+        }
+
+        this.dropdownClickOutside = this.dropdownClickOutside.bind(this);
+        this.displayHabitOptions = this.displayHabitOptions.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('click', this.dropdownClickOutside);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('click', this.dropdownClickOutside)
+    }
+    
+    dropdownClickOutside() {
+        if (this.state.dropdown) {
+            this.setState({ dropdown: false });
+        }
+    }
+
+    displayHabitOptions(e) {
+        e.stopPropagation();
+        this.setState({ dropdown: true });
+        const icon = e.currentTarget.querySelector('i');
+        const x = icon.getBoundingClientRect().x;
+        const y = icon.getBoundingClientRect().y;
+        const top = y + icon.offsetHeight + 5;
+        const right = window.innerWidth - x - icon.offsetWidth;
+        if (top === this.state.activeCoords.top && this.state.dropdown) {
+          this.setState({ dropdown: false });
+          return;
+        }
+        this.setState({ activeCoords: { top, right } });
+    }
+
+    render() {
+        const { storage, update, id, habits, setId } = this.props;
+
+        return (
+            <div className="habit-list">
+                <div className="container">
+                    <div className="row row--column">
+                    { this.state.dropdown &&
+                    <HabitOptions 
+                    storage={storage}
+                    update={update}
+                    coords={this.state.activeCoords}
+                    id={id} /> }
+                    {habits.map(habit => 
+                    <Habit key={ habit.id } habitOptions={habit}
+                    displayHabitOptions={this.displayHabitOptions}
+                    setId={setId} />)}
+                    </div>
+                </div>  
+            </div>
+        );
+    }
+}
+
+export default HabitList;
