f6cf386175fd65c1a1cd7f16d55400ca8806b028
Jefferson Valle <valleespinozajefferson@gmail.com>
2019-10-18 12:34:23 +0200

    Added value state to Select component

--- src/components/Select.js
+++ src/components/Select.js
@@ -6,7 +6,7 @@ function Select(props) {
         <div className="select" style={{width: props.width}}>
             <select className={"select__input" 
             + (props.className ? ' ' + props.className : '')}
-            onChange={props.onChange}>
+            onChange={props.onChange} value={props.value}>
                 {props.children}
             </select>
             <i class="select__icon fas fa-angle-down"></i>
