1bd108c7efaa1f17707f4eaa4dd69cd3bd5df0fa
Jefferson Valle <valleespinozajefferson@gmail.com>
2019-10-30 16:20:07 +0100

    Updates

---
+++ precache-manifest.3fe159111bc7df4fcf9aecec7c6d39d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464559ba356dc5b0ba466a6d7ed16d8a",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "f51c7da4291d986f426a",
+    "url": "/habit-tracker/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "d68466c7291ec469b1a6",
+    "url": "/habit-tracker/static/css/main.b756d113.chunk.css"
+  },
+  {
+    "revision": "f51c7da4291d986f426a",
+    "url": "/habit-tracker/static/js/2.81c00b61.chunk.js"
+  },
+  {
+    "revision": "d68466c7291ec469b1a6",
+    "url": "/habit-tracker/static/js/main.5f782dbb.chunk.js"
+  },
+  {
+    "revision": "f6303c9c1a360f4a7d12",
+    "url": "/habit-tracker/static/js/runtime-main.d0395a5f.js"
+  }
+]);
